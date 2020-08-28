d931d3b6ea31a8ac124780b0b3adc12bce871ae3
Jiamin Wang <jmechw@vt.edu>
2020-08-28 12:36:49 -0400

    Delete PathSetup.m

--- PathSetup.m
+++
@@ -1,7 +0,0 @@
-clear classes; clear all; close all; clc; restoredefaultpath;
-prjPath=pwd;
-addpath(genpath(prjPath));
-cd('../ANDY_V0_33/Library');
-libPath=pwd;
-addpath(genpath(libPath));
-cd(prjPath);
